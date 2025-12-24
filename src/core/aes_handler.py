262530b4474b3d6d017fefcde47d24f6918d2b52
Narin-Chhorn <esaranarin.chhorn@student.cadt.edu.kh>
2025-12-24 22:19:42 +0700

    Fixing small changes

--- src/core/aes_handler.py
+++ src/core/aes_handler.py
@@ -7,13 +7,23 @@ class AESHandler:
         return get_random_bytes(key_size)
     
     @staticmethod
-    def encrypt_data(data, key):
+    def encrypt_data(data, key, associated_data=None):
         cipher = AES.new(key, AES.MODE_GCM)
+        
+        # This means if someone changes the filename, the decryption will fail.
+        if associated_data:
+            cipher.update(associated_data)
+            
         ciphertext, tag = cipher.encrypt_and_digest(data)
         return cipher.nonce, ciphertext, tag
     
     @staticmethod
-    def decrypt_data(nonce, ciphertext, tag, key):
+    def decrypt_data(nonce, ciphertext, tag, key, associated_data=None):
         cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+        
+        # We must update with the EXACT same data used during encryption
+        if associated_data:
+            cipher.update(associated_data)
+            
         plaintext = cipher.decrypt_and_verify(ciphertext, tag)
         return plaintext
